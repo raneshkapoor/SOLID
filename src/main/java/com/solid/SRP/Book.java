15ece95c053634627ad229bae5535da9bfca32f4
ranesh <raneshkapoor@ymail.com>
2024-03-22 01:00:38 +0530

    Added SRP

---
+++ src/main/java/com/solid/SRP/Book.java
@@ -0,0 +1,33 @@
+package com.solid.SRP;
+
+public class Book {
+
+    private String code;
+
+    private String name;
+
+    private String description;
+
+    /*
+        Method related to Book functionality.
+     */
+    public String searchSentence(String sentence) {
+        return "";
+    }
+
+    /*
+        Method related to Book functionality.
+     */
+    public String getDetails() {
+        return "";
+    }
+
+    /*
+        Method not related to Book functionality.
+        Should not be in this class.
+     */
+    public void print() {
+
+    }
+
+}
---
+++ src/main/java/com/solid/SRP/SingleResponsibilityPrinciple.java
@@ -0,0 +1,9 @@
+package com.solid.SRP;
+
+public class SingleResponsibilityPrinciple {
+
+    public static void main(String[] args) {
+
+    }
+
+}
